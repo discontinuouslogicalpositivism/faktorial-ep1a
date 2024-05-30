e0eb70a5aec80b83961d8fbb33262a72a7d3f669
Lepka <158272809+discontinuouslogicalpositivism@users.noreply.github.com>
2024-05-30 09:46:10 +0200

    Add files via upload
    UKOL 8

---
+++ faktorial-ep1a/faktorial-ep1a/faktorial-ep1a/faktorial-ep1a/Program.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim cislo As Integer
+        Dim soucet As Integer = 0
+        Dim input As String
+        Dim ano As Boolean = False
+
+        While Not ano
+            Console.Write(" Zadej číslo (maximálně 10): ")
+            input = Console.ReadLine()
+
+            If Integer.TryParse(input, cislo) Then
+                If cislo <= 10 Then
+                    ano = True
+                Else
+                    Console.WriteLine("Číslo je příliš vysoké. Zadejte prosím číslo menší nebo rovno 10.")
+                End If
+            Else
+                Console.WriteLine("Neplatnej vstup. Zadejte prosím celé číslo.")
+            End If
+        End While
+
+        For i As Integer = 1 To cislo
+            soucet += i
+        Next
+
+        Console.WriteLine($" Součet od 1 do {cislo} je {soucet} ")
+
+    End Sub
+End Module
