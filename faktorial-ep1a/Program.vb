381696a7114d4f10b7b2d7f78a70d754dc771518
Štěpán Lepka <Lepka.Stepan@stud.vassboskovice.cz>
2024-05-30 09:27:39 +0200

    Přidejte soubory projektu.

---
+++ faktorial-ep1a/Program.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim cislo As Integer
+        Dim soucet As Integer = 0
+
+
+        Console.Write("Zadej číslo: ")
+        cislo = Console.ReadLine
+
+        For i = 1 To cislo
+            soucet = soucet + i
+        Next
+
+        Console.WriteLine($" Souèet od 1 do {cislo} je {soucet} ")
+
+    End Sub
+End Module
