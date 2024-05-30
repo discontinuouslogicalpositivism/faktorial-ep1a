0df2bc1df35c8c31ac0264bd1ff5d6df59e23c40
Lepka <158272809+discontinuouslogicalpositivism@users.noreply.github.com>
2024-05-30 09:58:19 +0200

    Add files via upload
    UKOL 10

--- faktorial-ep1a/faktorial-ep1a/Program.vb
+++ faktorial-ep1a/faktorial-ep1a/Program.vb
@@ -3,32 +3,41 @@
 Module Program
     Sub Main(args As String())
         Dim cislo As Integer
-        Dim soucet As Integer = 0
         Dim input As String
-        Dim ano As Boolean = False
+        Dim ano As Boolean
 
-        While Not ano
-            Console.Write("Zadej číslo (0 až 10): ")
-            input = Console.ReadLine()
+        While True
+            ano = False
+            Dim soucet As Integer = 0
 
-            If Integer.TryParse(input, cislo) Then
-                If cislo >= 0 And cislo <= 10 Then
-                    ano = True
-                ElseIf cislo < 0 Then
-                    Console.WriteLine("Záporné hodnoty NEJSOU povoleny!!!! Zadejte prosím číslo mezi 0 a 10.")
+            While Not ano
+                Console.Write("Zadej číslo (0 až 10): ")
+                input = Console.ReadLine()
+
+                If Integer.TryParse(input, cislo) Then
+                    If cislo >= 0 And cislo <= 10 Then
+                        ano = True
+                    ElseIf cislo < 0 Then
+                        Console.WriteLine("Záporné hodnoty NEJSOU povoleny!!!! Zadejte prosím číslo mezi 0 a 10.")
+                    Else
+                        Console.WriteLine("Číslo je příliš vysoké. Zadejte prosím číslo mezi 0 a 10.")
+                    End If
                 Else
-                    Console.WriteLine("Číslo je příliš vysoké. Zadejte prosím číslo mezi 0 a 10.")
+                    Console.WriteLine("Neplatný vstup. Zadejte prosím celé číslo.")
                 End If
-            Else
-                Console.WriteLine("Neplatný vstup. Zadejte prosím celé číslo.")
+            End While
+
+            For i As Integer = 1 To cislo
+                soucet += i
+            Next
+
+            Console.WriteLine($"Součet od 1 do {cislo} je {soucet}")
+
+            Console.WriteLine("Chcete zadat další číslo? (ano/ne): ")
+            Dim response As String = Console.ReadLine().ToLower()
+            If response <> "ano" Then
+                Exit While
             End If
         End While
-
-        For i As Integer = 1 To cislo
-            soucet += i
-        Next
-
-        Console.WriteLine($" Součet od 1 do {cislo} je {soucet} ")
-
     End Sub
 End Module
